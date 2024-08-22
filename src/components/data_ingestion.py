615603cd183c20aeb70048eb2f3c7d59f2b87fed
Swastik <swastiksanger@gmail.com>
2024-08-22 10:02:35 +0530

    Data Transformation Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,11 +7,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transform import DataTransformation
+from src.components.data_transform import DataTransformationConfig
+
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str=os.path.join('artifacts','train.csv')
-    test_data_path:str=os.path.join('artifacts','test.csv')
-    raw_data_path:str=os.path.join('artifacts',"data.csv")
+    train_data_path:str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
+    raw_data_path:str=os.path.join("artifacts","data.csv")
     
     
 class DataIngestion:
@@ -38,7 +42,7 @@ class DataIngestion:
                 self.ingestion_config.test_data_path
                     
                     
-                )
+            )
                 
         except Exception as e:
             raise CustomException(e,sys)
@@ -46,6 +50,9 @@ class DataIngestion:
             
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    data_tranformation=DataTransformation()
+    train_arr,test_arr,_=data_tranformation.initiate_data_transformation(train_data,test_data)
             
             
